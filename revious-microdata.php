053d8d1955ce2edf2c02912a6be471d58508515c
slim16165 <hyperreview@gmail.com>
2020-06-15 15:40:20 +0200

    Limit problem with xmlrpc

--- revious-microdata.php
+++ revious-microdata.php
@@ -24,8 +24,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 if (defined('DOING_AJAX') && DOING_AJAX)
     return;
 
+define( 'MY_PLUGIN_PATH', plugins_url( __FILE__ ) );
 
 
 	require_once("include/GenericShortcode.php");
 	require_once("include/site_specific/totaldesign_specific.php");
 
+
+
+function mmx_remove_xmlrpc_methods( $methods ) {
+    unset( $methods['system.multicall'] );
+    return $methods;
+}
+add_filter( 'xmlrpc_methods', 'mmx_remove_xmlrpc_methods');
