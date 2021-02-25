e5361c378bede90f8187efb9310eb8c1d74f91b3
slim16165 <hyperreview@gmail.com>
2021-02-25 10:43:56 +0100

    Integrato superinformati

--- include/site_specific/superinformati_specific.php
+++ include/site_specific/superinformati_specific.php
@@ -6,6 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 add_shortcode('link_analisi_sangue', 'link_analisi_sangue_handler_2');
 add_shortcode('sedi_inps', 'sedi_inps_handler');
+add_shortcode('link_vitamine', 'link_vitamine_handler');
+add_shortcode('link_diete', 'link_diete_handler');
 
 /**
  * @param $atts
@@ -50,6 +52,10 @@ function link_analisi_sangue_handler_2($atts, $content = null)
 	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/rdw-cv.htm", "RDW-CV e RDW-SD",
         "(variabilità della dimensione o del volume delle cellule dei globuli rossi; SD = deviazione standard; CV = coefficiente di variazione)");
 
+	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/ves-esami-valori.htm", "VES", "(velocità di elettrosedimentazione)");
+
+	
+
 	#endregion
 
     #region Piastrine
@@ -83,9 +89,362 @@ function link_analisi_sangue_handler_2($atts, $content = null)
 	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/analisi-del-sangue-in-gravidanza.htm", "Analisi del sangue in gravidanza", "");
 	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/transaminasi.htm", "Transaminasi", "");
 	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/carenza-di-ferro.htm", "Carenza di ferro", "");
+	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/gamma-gt-alte-e-basse.htm", "Gamma GT alte e basse", "");
+	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/iperglicemia.htm", "Iperglicemia", "");
+	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/anemia-falciforme.htm", "Anemia Falciforme", "");
+	$result .= $l->GetLinkWithImage("https://www.superinformati.com/medicina-e-salute/iperomocisteinemia.htm", "Iperomocisteinemia", "");
 
 	$result .= "</ul>";
 	return $result;
+}
+
+function link_vitamine_handler($atts, $content = null)
+{
+    $l = new ListOfPostsHelper(false, true, false);
+    $links_data = array(
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-a.htm",
+            'nome' => "Vitamina A"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamine-gruppo-b.htm",
+            'nome' => "Vitamine del gruppo B"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-b1.htm",
+            'nome' => "Vitamina B1"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-b2.htm",
+            'nome' => "Vitamina B2"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/medicina-e-salute/vitamina-b3.htm",
+            'nome' => "Vitamina B3"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/medicina-e-salute/vitamina-b5.htm",
+            'nome' => "Vitamina B5"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-b6.htm",
+            'nome' => "Vitamina B6"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/medicina-e-salute/vitamina-b8.htm",
+            'nome' => "Vitamina B8"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-b9.htm",
+            'nome' => "Vitamina B9"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-b12.htm",
+            'nome' => "Vitamina B12"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-c.htm",
+            'nome' => "Vitamina C"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-d-caratteristiche.htm",
+            'nome' => "Vitamina"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/carenza-vitamina-d.htm",
+            'nome' => "Vitamina D"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-e.htm",
+            'nome' => "Vitamina E"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-k.htm",
+            'nome' => "Vitamina K"
+        ),
+        array(
+            'target_url' => "https://www.superinformati.com/integratori/vitamina-k2.htm",
+            'nome' => "Vitamina K2"
+        )
+    );
+
+    $result = "<h3>Lista delle principali vitamine</h3>
+		<div class='thumbnail-list'>";
+
+    $result .= "<ul class='thumbnail-list'>";
+
+    $result .= $l->GetLinksWithImages($links_data);
+
+    $result .= "</ul></div>";
+    return $result;
+}
+
+
+function link_diete_handler($atts, $content = null)
+{
+    $l = new ListOfPostsHelper(false, true, false);
+    $links_data = array(
+
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-alcalina.htm",
+    'nome' => "Dieta Alcalina"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-dottor-nowzaradan.htm",
+    'nome' => "Dieta Dott. Nowzaradan"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-detox-menu-settimanale.htm",
+    'nome' => "Dieta Detox"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-scarsdale.htm",
+    'nome' => "Dieta Scarsdale"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-atkins.htm",
+    'nome' => "Dieta Atkins"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-digiuno-intermittente-16-8.htm",
+    'nome' => "Dieta Digiuno intermittente"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-ipocalorica.htm",
+    'nome' => "Dieta Ipocalorica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-louise-parker-menu-schema.htm",
+    'nome' => "Dieta Louise Parker"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-supermetabolismo.htm",
+    'nome' => "Dieta del Supermetabolismo"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-carb-lovers.htm",
+    'nome' => "Dieta Carb's Lover"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-disintossicante-fegato.htm",
+    'nome' => "Dieta disintossicante fegato"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/la-dieta-vegana.htm",
+    'nome' => "Dieta Vegana"
+),
+array(
+    'target_url' => "https://www.superinformati.com/medicina-e-salute/dieta-iposodica-menu.htm",
+    'nome' => "Dieta Iposodica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-lemme.htm",
+    'nome' => "Dieta Lemme"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-lampo.htm",
+    'nome' => "Dieta Lampo"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-anticellulite.htm",
+    'nome' => "Dieta anticellulite"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-del-riso.htm",
+    'nome' => "Dieta del riso"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-a-punti.htm",
+    'nome' => "Dieta a punti"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-veloce.htm",
+    'nome' => "Dieta veloce"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-mediterranea-e-cibi-sani.htm",
+    'nome' => "Dieta Mediterranea"
+),
+array(
+    'target_url' => "https://www.superinformati.com/maternita/dieta-in-gravidanza.htm",
+    'nome' => "Dieta in gravidanza"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-fruttariana.htm",
+    'nome' => "Dieta fruttariana"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/cibi-dietetici.htm",
+    'nome' => "Cibi dietetici"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-brasiliana.htm",
+    'nome' => "Dieta brasiliana"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-della-longevita.htm",
+    'nome' => "Dieta della longevità"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-dimagrante.htm",
+    'nome' => "Dieta dimagrante"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-dellacqua.htm",
+    'nome' => "Dieta dell'acqua"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-sirt.htm",
+    'nome' => "Dieta SIRT"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-panzironi-life-120.htm",
+    'nome' => "Dieta Panzironi"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-antiossidante.htm",
+    'nome' => "Dieta depurativa antiossidante"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/liposuzione-alimentare-dieta-blackburn.htm",
+    'nome' => "Dieta Blackburn"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-rina.htm",
+    'nome' => "Dieta Rina"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-fast.htm",
+    'nome' => "Dieta Fast 5.2"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-paleolitica.htm",
+    'nome' => "Dieta Paleolitica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-senza-glutine.htm",
+    'nome' => "Dieta senza glutine"
+),
+array(
+    'target_url' => "https://www.superinformati.com/medicina-e-salute/dieta-liquida.htm",
+    'nome' => "Dieta liquida"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-a-zona.htm",
+    'nome' => "Dieta a zona"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-plank-scopri-come-perdere-10-kg-in-un-mese.htm",
+    'nome' => "Perdere 10 kg con la Dieta Plank"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-senza-carboidrati-addio-pane-e-pasta.htm",
+    'nome' => "Dieta senza carboidrati"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-ferrea.htm",
+    'nome' => "Dieta Ferrea"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-plank.htm",
+    'nome' => "Dieta Plank"
+),
+array(
+    'target_url' => "https://www.superinformati.com/alimentazione/dieta-equilibrata.htm",
+    'nome' => "Dieta equilibrata"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/cristiano-ronaldo-dieta.htm",
+    'nome' => "Dieta Cristiano Ronaldo"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-lipofidica.htm",
+    'nome' => "Dieta Lipofidica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-macrobiotica.htm",
+    'nome' => "Dieta Macrobiotica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-gruppo-sanguigno.htm",
+    'nome' => "Dieta del gruppo sanguigno"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/diete-low-carb-iperproteiche.htm",
+    'nome' => "Dieta Low Carb"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/la-dieta-tisanoreica.htm",
+    'nome' => "Dieta Tisanoreica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-estiva.htm",
+    'nome' => "Dieta estiva"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-dukan-la-dieta-delle-polemiche.htm",
+    'nome' => "Dieta Dukan"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-drenante.htm",
+    'nome' => "Dieta drenante e sgonfiante"
+),
+array(
+    'target_url' => "https://www.superinformati.com/alimentazione/dieta-per-colesterolo-alto.htm",
+    'nome' => "Dieta per il colesterolo"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-chetogenica-funziona.htm",
+    'nome' => "Dieta chetogenica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-proteica.htm",
+    'nome' => "Dieta proteica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-iperproteica.htm",
+    'nome' => "Dieta iperproteica"
+),
+array(
+    'target_url' => "https://www.superinformati.com/dimagrire/dieta-mima-digiuno.htm",
+    'nome' => "Dieta Mima digiuno"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-tlc.htm",
+    'nome' => "Dieta TLC"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/perdere-peso-dieta-personalizzata.htm",
+    'nome' => "Dieta personalizzata"
+),
+array(
+    'target_url' => "https://www.superinformati.com/medicina-e-salute/ricette-dietetiche-per-dimagrire.htm",
+    'nome' => "Ricette dietetiche"
+),
+array(
+    'target_url' => "https://www.superinformati.com/alimentazione/alimentazione-corretta.htm",
+    'nome' => "Alimentazione corretta"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/le-15-diete-piu-famose-e-le-piu-efficaci.htm",
+    'nome' => "Le 33 diete più efficaci - articolone"
+),
+array(
+    'target_url' => "https://www.superinformati.com/diete/dieta-vegetariana-bilanciata-nellapporto-dei-nutrienti.htm",
+    'nome' => "Dieta vegetariana"
+)
+
+    );
+
+    $result = "<h3>Lista delle principali diete</h3>
+		<div class='thumbnail-list'>";
+
+    $result .= "<ul class='thumbnail-list'>";
+
+    $result .= $l->GetLinksWithImages($links_data);
+
+    $result .= "</ul></div>";
+    return $result;
 }
 
 
